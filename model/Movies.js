f00569f6e4dfc232671baddf0103d57c047742a8
Chibuzo Ekwue <chieezy5@gmail.com>
2023-02-02 17:26:42 +0100

    The movies schema

--- model/Movies.js
+++ model/Movies.js
@@ -2,6 +2,23 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const movieSchema = new Schema({
-	title: String,
-});
+const movieSchema = new Schema(
+	{
+		title: String,
+		overview: String,
+		original_langauage: String,
+		trailer: [{ type: String }],
+		poster: String,
+		backdrop: String,
+		vote_average: Number,
+		release_date: String,
+		genre: [{ type: String }],
+	},
+	{
+		timeStamps: true,
+	}
+);
+
+const Movie = mongoose.model("movies", movieSchema);
+
+export default Movie;
